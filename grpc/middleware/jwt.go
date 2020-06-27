239d465c8ef57cdd0425e00a88a09d600e51361f
Deep Dhillon <deep@dhillon.io>
2020-06-27 02:06:15 -0400

    Moved jwtValidator to middleware and fix pointer bug

--- grpc/middleware/jwt.go
+++ grpc/middleware/jwt.go
@@ -4,12 +4,56 @@ import (
 	"context"
 	"fmt"
 
+	jwt_go "github.com/dgrijalva/jwt-go"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"github.com/uptimize/go-utils/auth/jwt"
 	"google.golang.org/grpc"
 )
 
 // ValidateJwtFunc is a function that is provided to validate JWT request
 type ValidateJwtFunc func(context context.Context, server interface{}, fullMethod string) (context.Context, error)
+
+// JwtAuthService is an interface that is implemented to signal middleware to authenticate the request
+type JwtAuthService interface {
+	IsProtected() bool
+}
+
+// JwtAuthOverride is an interface that is implemented to overrride JWT authentication logic
+type JwtAuthOverride interface {
+	AuthImpl(manager *jwt.Manager, token string) (jwt_go.Claims, error)
+}
+
+// JwtValidator validates JWT requests when used inside JWT middleware
+func JwtMiddlewareValidator(manager *jwt.Manager) ValidateJwtFunc {
+	var newCtx context.Context
+	var claims jwt_go.Claims
+	var err error
+
+	return func(ctx context.Context, server interface{}, fullMethod string) (context.Context, error) {
+		if serviceAuth, ok := server.(JwtAuthService); ok {
+			if serviceAuth.IsProtected() {
+				// get token from the authentication header
+				authToken, err := grpc_auth.AuthFromMD(ctx, manager.GetHeaderScheme())
+				if err != nil {
+					return ctx, nil
+				}
+
+				if authOverride, ok := server.(JwtAuthOverride); ok {
+					claims, err = authOverride.AuthImpl(manager, authToken)
+				} else {
+					claims, err = manager.Verify(authToken)
+				}
+
+				newCtx = context.WithValue(ctx, "user", claims)
+			}
+		} else {
+			newCtx = ctx
+		}
+
+		return newCtx, err
+	}
+}
 
 // jwtMiddleware is grpc middleware for JWT authentication
 type jwtMiddleware struct {
